4f584e41d0f7ea288733906df0bca9483835220c
Chad Durkin <Chaddurkin@gmail.com>
2017-01-20 09:24:17 -0800

    add the third specification, if user inputs a number thats not divisible by 15 but is divisible by 5, output pong

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,25 @@
-var divideByFifteen = function(userNumber) {
+var divideBySpecs = function(userNumber) {
   if((userNumber % 15) === 0)
   {
     return "ping-pong";
+  }
+  else if((userNumber % 5) === 0)
+  {
+    return "pong";
   }
 
   return userNumber;
 };
 
 
-
-
 $(function() {
   $("#user-input").submit(function(event) {
     event.preventDefault();
     var userNumber = $("#user-number").val();
-    var userOutput;
 
-    userOutput = divideByFifteen(userNumber);
+    var userOutput = divideBySpecs(userNumber);
     console.log(userOutput);
-    
+
     $("#display-output").append("<li>" + userOutput + "</li>");
   })
 });
