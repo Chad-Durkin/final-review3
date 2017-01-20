bc363f7027fedd8dd0ae40dd6f2bb1e4f7ee03e3
Chad Durkin <Chaddurkin@gmail.com>
2017-01-20 09:46:22 -0800

    Add specification 5, making an array of numbers that count from 1-users number, updated Specifications list

--- js/scripts.js
+++ js/scripts.js
@@ -8,12 +8,27 @@ var divideBySpecs = function(userNumber) {
   return userNumber;
 };
 
+var createArray = function(userNumber) {
+  var numberArray = new Array(userNumber);
+  var numberCounter = 1;
+
+  for(index = 0; index < userNumber; index++)
+  {
+    numberArray[index] = numberCounter;
+    numberCounter++;
+  }
+
+  return numberArray;
+}
+
 
 $(function() {
   $("#user-input").submit(function(event) {
     event.preventDefault();
     var userNumber = $("#user-number").val();
-    var userOutput = divideBySpecs(userNumber);
+    var userOutput = createArray(userNumber);
+
+
     $("#display-output").append("<li>" + userOutput + "</li>");
   })
 });
